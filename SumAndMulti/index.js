baf6e11e0a1c3f3c124958cbf48392a69588d75f
Bartlomiej Korytkowski <korytkowskibartlomiej@gmail.com>
2020-10-05 19:26:04 +0200

    prepare new JS

--- SumAndMulti/index.js
+++ SumAndMulti/index.js
@@ -1,31 +1,14 @@
-var a = 1;
-var b = 2;
-var c = 3;
-var d = 4;
-
-var table = [a, b, c, d];
-
-function countNumber(a, b, c, d) {
-    var sum = 0;
-    var mmulti = 1;
-
-    for (var i = 0; i < table.length; i++) {
-        sum += table[i];
-        mmulti *= table[i];
-    }
-}
-
 function finishCount() {
-    var a = document.getElementById("firstNumber").value;
-    var b = document.getElementById("secondNumber").value;
-    var c = document.getElementById("thirdNumber").value;
-    var d = document.getElementById("fourthNumber").value;
+    let writeNumber = [
+        document.getElementById("firstNumber").value,
+        document.getElementById("secondNumber").value,
+        document.getElementById("thirdNumber").value,
+        document.getElementById("fourthNumber").value
+    ];
 
-    var counting = countNumber(a, b, c, d);
-    var resultCounting = counting;
+    let sum = Number(writeNumber[0]) + Number(writeNumber[1]) + Number(writeNumber[2]) + Number(writeNumber[3]);
+    document.getElementById("sumMessage").innerHTML = sum;
 
-    document.getElementById("message").innerHTML = resultCounting;
+    let multi = Number(writeNumber[0]) * Number(writeNumber[1]) * Number(writeNumber[2]) * Number(writeNumber[3]);
+    document.getElementById("multiMessage").innerHTML = multi;
 }
-
-// console.log("Suma: " + sum);
-// console.log("Iloczyn: " + mmulti);
